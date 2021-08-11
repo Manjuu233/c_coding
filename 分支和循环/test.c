84792bf4b0f2955f62b0ab82164caaae9fbc5f22
nagiasu <manjuu233@outlook.com>
2021-08-12 00:35:46 +0800

    if else

---
+++ 分支和循环/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	//int age = 10;
+	//if (age < 18)
+	//	printf("未成年\n");
+	//else if(age>=18&&age<28)
+	//	printf("青年\n");
+	//else if (age >= 28 && age < 50)
+	//	printf("壮年\n");
+	//else
+	//	printf("老年\n");
+	//return 0;
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+	else
+			printf("haha\n");
+	return 0;
+}
