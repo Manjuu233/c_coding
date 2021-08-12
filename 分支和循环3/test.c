dffa7a8efc7464ee7e2261ec734c678a8ac0d266
nagiasu <manjuu233@outlook.com>
2021-08-12 22:03:38 +0800

    getchar

--- 分支和循环3/test.c
+++ 分支和循环3/test.c
@@ -2,17 +2,38 @@
 #include <stdio.h>
 int main()
 {
-	int i = 1;
+	/*int i = 1;
 	while (i <= 10)
 	{
 		if (i == 5)
 			continue;
 		printf("%d\n", i);
 		i++;
-	}
+	}*/
 	//{
 	//	printf("%d\n", i);
 	//	i++;
 	//}
+
+	//int ch = 0;
+	//while ((ch = getchar()) != EOF)
+	//	putchar(ch);
+	//return 0;
+
+	int ret = 0;
+	char passwd[20] = { 0 };
+	printf("请输入密码：>");
+	scanf("%s",passwd );
+	printf("确认？");
+	getchar();//为什么要多一个getchar？
+	ret = getchar();
+	if (ret == 'Y')
+	{
+		printf("已确认");
+	}
+	else
+	{
+		printf("取消确认");
+	}
 	return 0;
 }
