7db72c34f005a05cb112e1a0d3d194b3bf42d9b6
nagiasu <manjuu233@outlook.com>
2021-08-20 02:53:45 +0800

    for的变种_1

---
+++ 分支和循环6/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	//for (;;)
+	//{
+	//	printf("hehe\n");
+	//}
+	//return 0;
+	int i = 0;
+	int j = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			printf("hehe\n");
+		}
+	}
+	return 0;
+}
