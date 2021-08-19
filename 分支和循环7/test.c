9d9251d917354700fdf7eb7a607c5992c8e4a436
nagiasu <manjuu233@outlook.com>
2021-08-20 03:27:38 +0800

    for的变种_2

---
+++ 分支和循环7/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	//int x, y;
+	//for (x = 0, y = 0; x < 2 && y < 5; ++x, ++y)
+	//{
+	//	printf("hehe\n");
+	//}
+	int i = 0;
+	int k = 0;
+	for (i = 0, k = 0; k = 0; i++, k++)
+	{
+		k++;
+		printf("%d\n", k);
+	}
+	return 0;
+}
